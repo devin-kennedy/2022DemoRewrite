985b6dcf0536f274bd6c32b791e825cd0456d49a
Llaaamaguy <67341568+Llaaamaguy@users.noreply.github.com>
2022-05-19 08:55:40 -0400

    k

--- app/src/main/java/com/dkennedy/a2022demorewrite/MainActivity.kt
+++ app/src/main/java/com/dkennedy/a2022demorewrite/MainActivity.kt
@@ -1,3 +1,17 @@
+//   Copyright 2022 Devin Kennedy
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+
 package com.dkennedy.a2022demorewrite
 
 import android.content.pm.PackageManager
@@ -52,7 +66,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun start_camera() {
         val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
-        val handDetectionModel = HandDetectionModel("object_labeler")
+        val handDetectionModel = HandDetectionModel("2060-30E-2")
 
 
         cameraProviderFuture.addListener({
